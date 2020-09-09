86c9b0f986e695e7b5c06028f33568a9c59c4ccf
mggoaaa <mggoaaa@gmail.com>
2020-09-09 13:04:07 -0500

    Comportamiento2

---
+++ Agente1/Comportamiento2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agente1;
+import jade.core.behaviours.Behaviour;
+/**
+ *
+ * @author mggo9
+ */
+public class Comportamiento2 extends Behaviour {
+    int numero=0;
+    public void action(){
+        
+        System.out.print(" " + numero);
+        numero++;
+    }
+            
+    public boolean done(){
+        if(numero<=100)
+            return false;
+        else
+            return true;
+    }
+
+}
